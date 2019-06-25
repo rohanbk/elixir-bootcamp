1516db4abdc5cf8b8421e5edd02ffdccabec06fa
Rohan Balakrishnan <rohanbk@gmail.com>
2019-06-25 07:45:16 -0700

    First commit

---
+++ cards/lib/cards.ex
@@ -0,0 +1,21 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
+  end
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @spec contains?(any, any) :: boolean
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
+
