cb1e7175da7059de89c2d6cbdb80a5ca32b38308
Rohan Balakrishnan <rohanbalakrishnan@Rohans-MacBook-Pro.local>
2019-06-25 14:27:36 -0700

    feat(card functions): Add deal, save, read funcs

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -16,5 +16,20 @@ defmodule Cards do
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
+  def deal(deck, cards_needed) do
+    Enum.split(deck, cards_needed)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename,binary)
+  end
+
+  def read(filename) do
+    {_status, binary} = File.read(filename)
+    :erlang.binary_to_term(binary)
+  end
+
 end
 
